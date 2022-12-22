ae4213afeeb867e6e542379c922c3233db2bf40b
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-12-21 22:27:28 -0300

    feat: Transaction page and  Header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components';
+import { Transaction } from './pages/Transactions';
 import { GlobalStyles } from './styles/global';
 import { defaultTheme } from './styles/themes/defaut';
 
@@ -6,6 +7,8 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
+
+      <Transaction />
     </ThemeProvider>
   );
 }
